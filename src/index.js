9c88055a52c1e639d2b3967ffe7abadf7fe9487c
Marcin <marcin@opensoftware.pl>
2020-01-15 15:17:36 +0100

    [v0.0.2] Export eventBus from package

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ require('@/config/loader')
 import { resolveZipFile, exportToZip } from '@/zip_resolver'
 import { renderForm } from '@/form_renderer'
 import { createSchemaFromForm } from '@/create_schema_from_form'
+import { EventHandlerConstant, eventBus } from '@/template/handler/event_handler'
 import PreviewComponent from '@/template/ui/PreviewComponent'
 import FormBuilder from '@/FormBuilder'
 
-export { 
+export {
   resolveZipFile, exportToZip,
   renderForm,
   createSchemaFromForm,
+  EventHandlerConstant, eventBus,
   PreviewComponent,
   FormBuilder
 }
