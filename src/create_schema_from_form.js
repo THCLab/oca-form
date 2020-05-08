40af184f935d3cf267ce665777ab2a5946a2924c
Marcin <marcin@opensoftware.pl>
2020-05-08 15:30:09 +0200

    Add translations validation before creating schema from form

--- src/create_schema_from_form.js
+++ src/create_schema_from_form.js
@@ -25,6 +25,8 @@ export const createSchemaFromForm = (baseForm, form) => {
         })
     }
 
+    const defaultTranslation = form.translations[0]
+
     form.sections.forEach((section, sectionIndex) => {
       section.row.controls.forEach(control => {
           if (control.attrName.length <= 0) {
@@ -39,6 +41,26 @@ export const createSchemaFromForm = (baseForm, form) => {
           form.translations.forEach(translation => {
             const translationFields = HashMap_init()
             const translationControl = translation.data.controls.find(tC => tC.fieldName == control.fieldName)
+
+            if (translation.language != defaultTranslation.language) {
+              const defaultTranslationControl = defaultTranslation.data.controls.find(tC => tC.fieldName == control.fieldName)
+              if(defaultTranslationControl.label.length > 0 &&
+                translationControl.label.length == 0) {
+                throw `Missing ${translation.language} translation for '${control.attrName}' label`
+              }
+              if(defaultTranslationControl.information.length > 0 &&
+                translationControl.information.length == 0) {
+                throw `Missing ${translation.language} translation for '${control.attrName}' information`
+              }
+              if(defaultTranslationControl.dataOptions.length > 0) {
+                translationControl.dataOptions.forEach((op, index) => {
+                  if(op.text.length == 0) {
+                    throw `Missing ${translation.language} translation for ${index + 1}. element in '${control.attrName}' options`
+                  }
+                })
+              }
+            }
+
             const category = translation.data.sections[sectionIndex].label
             const categories = [category]
 
