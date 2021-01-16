c12e3ba0dfb324c4b881b6b594182a56c19b65a5
blelump <blelump@martex.nom.pl>
2021-01-16 10:35:55 +0100

    Add dev app

---
+++ dev-app/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="q-app">
+    <main-layout />
+  </div>
+</template>
+<script>
+
+import MainLayout from 'layouts/MainLayout.vue'
+export default {
+  components: { MainLayout},
+  name: 'App'
+}
+</script>
